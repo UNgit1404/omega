3d8f39afad8e20f90bd0ef2b0bc827d3279ce5c8
UNgit1404 <101168008+UNgit1404@users.noreply.github.com>
2023-08-13 19:44:12 +0000

    Updated contracts

--- truffle/contracts/Token.sol
+++ truffle/contracts/Token.sol
@@ -4,22 +4,58 @@ pragma solidity ^0.8.0;
 import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
 import "@openzeppelin/contracts/access/Ownable.sol";
 
-contract Token is ERC20, Ownable {
+contract ExpirableToken is ERC20, Ownable {
     struct ExpiryInfo {
         uint256 expiryTimestamp;
         bool isExpired;
     }
 
     mapping(address => ExpiryInfo) public expiryOf;
+    mapping(address => bool) public whitelistedTransfers; // Addresses that can transfer tokens
+    address[] public whitelistAddresses;
 
-    constructor(string memory name, string memory symbol, uint256 initialSupply) ERC20(name, symbol) {
-        _mint(msg.sender, initialSupply * (10 ** uint256(decimals())));
+    constructor(string memory name, string memory symbol) ERC20(name, symbol) {
     }
 
-    function mint(address account, uint256 amount, uint256 expiryTimestamp) public onlyOwner {
+    modifier onlyTransferAllowed() {
+        require(whitelistedTransfers[msg.sender], "Sender is not allowed to transfer");
+        _;
+    }
+
+    modifier onlyOwnerOrWhitelist() {
+        require(msg.sender == owner() || whitelistedTransfers[msg.sender], "Sender is not allowed");
+        _;
+    }
+
+    function addToTransferWhitelist(address account) public onlyOwner {
+        whitelistedTransfers[account] = true;
+    }
+
+    function removeFromTransferWhitelist(address account) public onlyOwner {
+        whitelistedTransfers[account] = false;
+    }
+
+    function addAddressToWhitelist(address account) public onlyOwner {
+        require(!whitelistedTransfers[account], "Address is already whitelisted");
+        whitelistAddresses.push(account);
+        whitelistedTransfers[account] = true;
+    }
+
+    function getWhitelistAddresses() public view returns (address[] memory) {
+        return whitelistAddresses;
+    }
+
+    function mintToWhitelist(uint256 amount, uint256 expiryTimestamp) public onlyOwnerOrWhitelist {
         require(expiryTimestamp > block.timestamp, "Expiry must be in the future");
-        _mint(account, amount);
-        expiryOf[account] = ExpiryInfo(expiryTimestamp, false);
+        require(whitelistAddresses.length > 0, "Whitelist is empty");
+
+        uint256 individualAmount = amount / whitelistAddresses.length;
+
+        for (uint256 i = 0; i < whitelistAddresses.length; i++) {
+            address recipient = whitelistAddresses[i];
+            _mint(recipient, individualAmount);
+            expiryOf[recipient] = ExpiryInfo(expiryTimestamp, false);
+        }
     }
 
     function burn(address account, uint256 amount) public onlyOwner {
@@ -27,16 +63,15 @@ contract Token is ERC20, Ownable {
         expiryOf[account].isExpired = true;
     }
 
-    function transfer(address recipient, uint256 amount) public override returns (bool) {
+    function transfer(address recipient, uint256 amount) public override onlyTransferAllowed returns (bool) {
         require(!expiryOf[msg.sender].isExpired, "Sender's tokens have expired");
         require(!expiryOf[recipient].isExpired, "Recipient's tokens have expired");
         return super.transfer(recipient, amount);
     }
 
-    function transferFrom(address sender, address recipient, uint256 amount) public override returns (bool) {
+    function transferFrom(address sender, address recipient, uint256 amount) public override onlyTransferAllowed returns (bool) {
         require(!expiryOf[sender].isExpired, "Sender's tokens have expired");
         require(!expiryOf[recipient].isExpired, "Recipient's tokens have expired");
         return super.transferFrom(sender, recipient, amount);
     }
 }
-//hi
